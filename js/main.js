fac97338b2717503894a89c55d3e056d9c0fcfde
giuliana2043 <giulianna.2043@gmail.com>
2017-01-13 05:10:47 -0500

    no entiendo

---
+++ js/main.js
@@ -0,0 +1,6 @@
+    var Ingresa= document.getElementById("form-signin-heading");
+	Ingresa.innerHTML="Ingresa tus datos";
+	var recordar = document.getElementsByTagName("span");	
+	recordar.innerHTML="Recordar";
+	var enterMail=document.getElementById("inputEmail").placeholder="Ingresa tu correo";
+	var enterPassword=document.getElementById("inputPassword")
